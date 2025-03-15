862ae781f38987bc3d55edd1d2675b587b965e9a
socical <socical.dev@gmail.com>
2025-03-16 02:11:39 +0900

    admin 페이지를 좀 더 편하기 보기 위해 한국어로 변환

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from todo.models import Todo
 
-# Register your models here.
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'start_date', 'end_date')
