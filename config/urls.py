d3164c0288f1037a4685990d9bca2e30ba80a36c
socical <socical.dev@gmail.com>
2025-03-17 16:16:38 +0900

    🌱 ✨feat: chapter_04 과제 업로드
    Form, ORM 을 이용한 CRUD 구현

--- config/urls.py
+++ config/urls.py
@@ -1,29 +1,16 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from todo.views import get_todo_list, get_todo_info
-from users import views as users_views
+from todo.views import todo_list, todo_info, todo_create, todo_update, todo_delete
+from users import views as user_views
 
 urlpatterns = [
-    path('todo/', get_todo_list, name='todo_list'),
-    path('todo/<int:todo_id>/', get_todo_info, name='todo_info'),
+    path('todo/', todo_list, name='todo_list'),
+    path('todo/create/', todo_create, name='todo_create'),
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+    path('todo/<int:todo_id>/update/', todo_update, name='todo_update'),
+    path('todo/<int:todo_id>/delete/', todo_delete, name='todo_delete'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/login/', users_views.login, name='login'),
-    path('accounts/signup/', users_views.sign_up, name='signup')
+    path('accounts/login/', user_views.login, name='login'),
+    path('accounts/signup/', user_views.sign_up, name='signup')
 ]
--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
+
 class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=50)
     description = models.TextField()
     start_date = models.DateField()
@@ -9,10 +13,6 @@ class Todo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # 어떤 정보인지 한 눈에 알아보기 위해 사용
+    # 어떤 정보인지 편하게 알아보기 위해 사용
     def __str__(self):
         return self.title
-
-    class Meta:
-        verbose_name = 'Todo'
-        verbose_name_plural = 'Todo 리스트'
